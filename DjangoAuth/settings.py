417790edcc1b34bf9c035745c6f53b0b88cd79db
Srikant Shubham <srikantsubham10000@gmail.com>
2023-05-06 13:20:28 +0530

    debug

--- DjangoAuth/settings.py
+++ DjangoAuth/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'f)!tm9&$wiue!bzecy58ql1tlas+gcae0s0b6^a7yt37$u!en+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
