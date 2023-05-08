3b2cc1b118eb1b2db6150d275a8844657c0b6075
Srikant Shubham <srikantsubham10000@gmail.com>
2023-05-08 10:41:57 +0530

    urls exempt

--- DjangoAuth/urls.py
+++ DjangoAuth/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from django.contrib import admin
 from authen import views
+from django.views.decorators.csrf import csrf_exempt
+
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login/', auth_views.LoginView.as_view(template_name='auth/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('signup/', views.signup, name='signup'),
+    path('login/', csrf_exempt(auth_views.LoginView.as_view(template_name='auth/login.html')), name='login'),
+    path('logout/', csrf_exempt(auth_views.LogoutView.as_view()), name='logout'),
+    path('signup/', csrf_exempt(views.signup), name='signup'),
     path('admin/', admin.site.urls),
 ]
